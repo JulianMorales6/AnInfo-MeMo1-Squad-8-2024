62702049c1ec0262cc0e943d215bb6b943565d1b
julianmorales6 <jlmorales@fi.uba.ar>
2024-06-20 20:58:27 -0300

    changed util list in proyectApp

--- src/main/java/com/aninfo/ProyectApp.java
+++ src/main/java/com/aninfo/ProyectApp.java
@@ -1,8 +1,6 @@
 package com.aninfo;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
