fecb0a5b536a08e64e541f3108857af1cd7bc9ee
julianmorales6 <jlmorales@fi.uba.ar>
2024-06-24 05:39:39 -0300

    new tests

--- src/test/java/com/aninfo/integration/cucumber/StepDefinitions.java
+++ src/test/java/com/aninfo/integration/cucumber/StepDefinitions.java
@@ -2,12 +2,15 @@ package com.aninfo.integration.cucumber;
 
 import java.util.Collection;
 
+import org.junit.After;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.aninfo.model.Project;
+import com.aninfo.model.Task;
 import com.aninfo.service.ProjectService;
+import com.aninfo.service.ResourceService;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
@@ -18,8 +21,12 @@ public class StepDefinitions{
 
     @Autowired
     private ProjectService projectService;
-    
+    @Autowired
+    private ResourceService resourceService;
+
     private Project project = null;
+    private Project mock_project = new Project("title","description");
+    private Task task = null;
 
     @Given("There are no projects in the repository")
     public void there_are_no_projects_in_the_repository() {
@@ -45,6 +52,45 @@ public class StepDefinitions{
     @And("Project description is {string}")
     public void project_description_is(String description) {
         assertEquals(description, project.getDescription());
+    }
+
+    @Given("There is a leader with id {long}")
+    public void resource_with_id_exists(Long resource_id) {
+        assertEquals(true, resourceService.resourceExists(resource_id)); 
+    }
+
+    @Given("There is a project with a leader with id {long}")
+    @When("I assign the leader with id {long} to project")
+    public void assign_resource_to_project(Long resource_id) {
+        if(resourceService.resourceExists(resource_id)) {
+            mock_project.assignLeader(resource_id);
+        }
+    }
+
+    @Then("Leader with id {long} is assigned to project")
+    public void project_has_leader(Long resource_id) {
+        assertEquals(resource_id, mock_project.getAssignedLeader());
+    }
+
+    @Given("There is not a leader with id {long}")
+    public void resource_with_id_does_not_exist(Long resource_id) {
+        assertEquals(false, resourceService.resourceExists(resource_id)); 
+    }
+    
+    @Then("Leader with id {long} is not assigned to project")
+    public void project_does_not_have_leader(Long resource_id) {
+        assertEquals(null, mock_project.getAssignedLeader());
+    }
+
+    @Given("There is a project with id 1") 
+    public void there_is_project(Long project_id) {
+        assertEquals(1L, projectService.findById(project_id));
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Tearing down after scenario execution...");
+        mock_project = new Project("title","description");
     }
 
 }
--- src/test/resources/cucumber/project_operations.feature
+++ src/test/resources/cucumber/project_operations.feature
@@ -8,3 +8,17 @@ Feature: Project operations
     And Project name is "Quantum Dawn"
     And Project description is "computing solutions for secure data processing"
 
+  Scenario: Successfully assign leader to project
+    Given There is a leader with id 1
+    When I assign the leader with id 1 to project
+    Then Leader with id 1 is assigned to project
+
+  Scenario: Cannot assign unavailable leader to project
+    Given There is not a leader with id 4
+    When I assign the leader with id 4 to project
+    Then Leader with id 4 is not assigned to project
+
+  Scenario: Change leader of project
+    Given There is a project with a leader with id 1
+    When I assign the leader with id 2 to project
+    Then Leader with id 2 is assigned to project
